a1edbbb7a6081bd18b6b923db708bd0b39931199
habibGM <72284417+GMhabib@users.noreply.github.com>
2023-06-23 21:15:22 +0700

    Update habibvictory.js

--- habibvictory.js
+++ habibvictory.js
@@ -1,5 +1,9 @@
 <!-- habib inject victory script -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.min.js"></script>
 <script src="https://unpkg.com/victory-core@9.1.1/dist/victory-core.js"></script>
 <script src="https://unpkg.com/victory-chart@13.1.1/dist/victory-chart.js"></script>
+import defaultExport from "https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js";
+import defaultExport from "https://cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.min.js";
+import defaultExport from "https://unpkg.com/victory-core@9.1.1/dist/victory-core.js";
+import defaultExport from "https://unpkg.com/victory-chart@13.1.1/dist/victory-chart.js";
