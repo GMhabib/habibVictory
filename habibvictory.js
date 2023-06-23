fed8c11f264ac3d371e11715d190fd0c952f953b
habibGM <72284417+GMhabib@users.noreply.github.com>
2023-06-23 21:05:15 +0700

    Create habibvictory.js

---
+++ habibvictory.js
@@ -0,0 +1,5 @@
+<!-- habib inject victory script -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/react/15.3.2/react-dom.min.js"></script>
+<script src="https://unpkg.com/victory-core@9.1.1/dist/victory-core.js"></script>
+<script src="https://unpkg.com/victory-chart@13.1.1/dist/victory-chart.js"></script>
